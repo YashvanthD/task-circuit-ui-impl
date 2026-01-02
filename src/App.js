753ee40b5b4e2debed6e393eae3ebce0436caadc
YashvanthD <yashvanthsira@gmail.com>
2026-01-02 13:34:54 +0530

    Add ErrorBoundary component to handle errors in AppRoutes

--- src/App.js
+++ src/App.js
@@ -67,10 +67,34 @@ function AppRoutes() {
   );
 }
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("ErrorBoundary caught an error", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
 export default function App() {
   return (
     <Router>
-      <AppRoutes />
+      <ErrorBoundary>
+        <AppRoutes />
+      </ErrorBoundary>
     </Router>
   );
 }
